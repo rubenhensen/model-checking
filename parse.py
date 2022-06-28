defa8dee350848e9dacc2320bfe8616d9aebcf41
Ruben Hensen <ruben.hensen@pm.me>
2022-06-28 07:19:19 +0000

    project cleanup

--- parse.py
+++ parse.py
@@ -1,90 +1,51 @@
-# Parses the crazy simulator format of stormpy to this:
-# [
-# [
-# (State, label, State),
-# (State, label, State),
-# (State, label, State),
-# ],
-# [
-# (State, label, State),
-# (State, label, State),
-# (State, label, State),
-# ],
-# [
-# (State, label, State),
-# (State, label, State),
-# (State, label, State),
-# ]
-# ]
-
 import json
-import re
 
 class State:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, state):
+        self.state = state
 
     def __eq__(self, other):
         if isinstance(other, State):
-            return self.x == other.x and self.y == other.y
+            return self.state == other.state
         return False
     
     def __lt__(self, other):
         if isinstance(other, State):
-            if self.x == other.x:
-                return self.y < other.y
-            if self.x > other.x:
-                return False
-            if self.x < other.x:
-                return True 
+            if self.state < other.state:
+                return True
         return False
         
 
     def __repr__(self):
-        return f"State(x: {self.x}, y: {self.y})"
+        return f"State({self.state})"
 
     def __key(self):
-        return (self.x, self.y)
+        return (self.state)
 
     def __hash__(self):
         return hash(self.__key())
 
 def grid_parse(file_path):
-    file_text = ''
-    with open(file_path, 'rt') as file_placeholder:
-        lines = file_placeholder.readlines()
-        file_text = '\n'.join(lines)  # This provides the whole file data as string
+    f = open(file_path)
+
+    data = json.load(f)
+
+    coupled = data['coupled']
+    paths = data['paths']
+    transitions = []
+    for trace in paths:
+        tracelist = []
+        for id, state in enumerate(trace):
+            if id == len(trace) -1:
+                break 
+            st1 = str(state).replace("\n", "")
+            st1 = str(st1).replace(" ", "")
+            st2 = str(trace[id+1]).replace("\n", "")
+            st2 = str(st2).replace(" ", "")
+            tracelist.append((State(st1),State(st2)))
+        transitions.append(tracelist)
+    return (coupled, transitions)
 
 
-    arr = re.split(r'\[|\]', file_text)
-    arr = arr[2:-2:2]
-    l1 = []
-    for idx, trace in enumerate(arr):
-        l2 = []
-        line = re.split(r'\r?\n', trace)
-        # print(f"-------{idx}\n")
-        for idx, line in enumerate(line):
-            matches = re.findall('\d|north|west|east|south', line, re.DOTALL)
-            # print(matches)
-            if matches:
-                if len(matches) == 2:
-                    l2.append(State(matches[0], matches[1]))
-                else:
-                    l2.append(matches[0])
-        l1.append(l2)
-
-    l3 = []
-    for tr in l1:
-        trace = []
-        length = len(tr)
-        for i, item in enumerate(tr):
-            if length - i < 3:
-                break
-            if i % 2 == 0:
-                trace.append((tr[i],tr[i+1],tr[i+2]))
-        l3.append(trace)
-            # if i % 2 == 1:
-            #     print()
-        
-    return l3
+if __name__ == '__main__':
+    grid_parse("export_simulator.txt")
