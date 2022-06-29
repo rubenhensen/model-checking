d144586d3fd3e096f96e80d0750aee54787d9089
Ties <tg.klappe@gmail.com>
2022-06-29 10:30:53 +0000

    Runs formula on original

--- main.py
+++ main.py
@@ -22,28 +22,27 @@ def reachability_probability(dtmc):
 
 
 prism_model_path = "prism_models/grid_5x5.prism"
-(original_prism_program, transitions) = grid_simulator_deterministic(500, 1000, prism_model_path) # Writes to file, uncomment to generate new file
 
+(original_prism_program, transitions) = grid_simulator_deterministic(500, 1000, prism_model_path)
 
-# original_properties = stormpy.parse_properties(formula_str, original_prism_program)
-# original_model = stormpy.build_model(original_prism_program, original_properties)
-# original_result = stormpy.model_checking(original_model,original_properties[0])
+original_properties = stormpy.parse_properties(formula_str, original_prism_program)
+original_model = stormpy.build_symbolic_model(original_prism_program, original_properties)
+original_result = stormpy.model_checking(original_model,original_properties[0])
+original_filter = stormpy.create_filter_initial_states_symbolic(original_model)
+original_result.filter(original_filter)
 
-# filter = stormpy.create_filter_initial_states_sparse(original_model)
-# original_result.filter(filter)
+print(f"Result original model: {original_result}")
 
-# print(f"Result original model: {original_result}")
-
-(variables, coupled, traces) = grid_parse("export_simulator.txt") 
+(variables, coupled, traces) = grid_parse("export_simulator.txt")
 
 # approx0 = frequentist(traces)
-approx1 = frequentist_coupled(coupled, traces)
+# approx1 = frequentist_coupled(coupled, traces)
 # approx2 = bayesian_iter([], traces)
 # approx3 = bayesian_iter(coupled, traces, transitions)
 
-print(approx1)
+# print(approx1)
 
 # print(f"Frequentist: {formula_str} {reachability_probability(generate_model(approx0))}")
 # print(f"Frequentist coupled: {formula_str} {reachability_probability(generate_model(approx1))}")
 # print(f"Bayesian: {formula_str} {reachability_probability(generate_model(approx2))}")
-print(f"Bayesian coupled: {formula_str} {reachability_probability(generate_model2(approx1, prism_model_path, variables))}")
+# print(f"Bayesian coupled: {formula_str} {reachability_probability(generate_model2(approx1, prism_model_path, variables))}")
